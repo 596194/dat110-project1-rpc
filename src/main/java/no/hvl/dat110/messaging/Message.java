2aa71bfc17ba9fc5c4e25cd59c47085c17cd5ef4
596194 <tord.h.pedersen@gmail.com>
2023-01-31 15:31:24 +0100

    en/decapsulate tests ok

--- src/main/java/no/hvl/dat110/messaging/Message.java
+++ src/main/java/no/hvl/dat110/messaging/Message.java
@@ -1,25 +1,52 @@
 package no.hvl.dat110.messaging;
 
-import no.hvl.dat110.TODO;
+import java.util.Arrays;
 
 public class Message {
 
 	// the up to 127 bytes of data (payload) that a message can hold
 	private byte[] data;
+//	private int payLoadIndex;
 
 	// construction a Message with the data provided
 	public Message(byte[] data) {
-		
+
 		// TODO - START
-		
-		if (true)
-			throw new UnsupportedOperationException(TODO.constructor("Message"));
-			
-		// TODO - END
+//		check if data is not null and not longer than 127 bytes
+		if (data != null && data.length <= 127) {
+
+//			this.payLoadIndex= data.length;
+
+//			copying data into this.data
+//			this.data = Arrays.copyOf(data,data.length);
+			this.data = data;
+
+
+
+//			makes this.data into a fixed byte array with length 127
+//			this.data = new byte[127];
+//			copying data into this.data, leaving padding empty
+//			int i = 0;
+//			while (i < data.length) {
+//				this.data[i] = data[i];
+//				i++;
+//			}
+		}
+//		else{
+//			throw new IllegalArgumentException("data null or too long");
+//		}
+//		 TODO - END
+
 	}
 
 	public byte[] getData() {
 		return this.data; 
 	}
 
+	public void setData(byte[] data) {
+		this.data = data;
+	}
+//	public byte getPayloadIndex(){
+//		return (byte) this.payLoadIndex;
+//	}
 }
--- src/main/java/no/hvl/dat110/messaging/MessageUtils.java
+++ src/main/java/no/hvl/dat110/messaging/MessageUtils.java
@@ -12,32 +12,70 @@ public class MessageUtils {
 	public static String MESSAGINGHOST = "localhost";
 
 	public static byte[] encapsulate(Message message) {
-		
-		byte[] segment = null;
+
+		byte[] segment=new byte[SEGMENTSIZE];
 		byte[] data;
-		
+
 		// TODO - START
-		
+
 		// encapulate/encode the payload data of the message and form a segment
 		// according to the segment format for the messaging layer
-		
-		if (true)
-			throw new UnsupportedOperationException(TODO.method());
-			
+		data = message.getData();
+
+//		setting first byte if segment as the payload
+//		segment[0] = message.getPayloadIndex(); //
+//		trying again:
+
+//		this.data = Arrays.copyOf(data,data.length);
+
+//		segment = Arrays.copyOf(data,127);
+		segment[0]= (byte) data.length;
+		int i=0;
+		while(i<data.length){
+			segment[i + 1] = data[i];
+			i++;
+		}
+
+
+//		copy data into segment from index 1
+//		for (int i = 0; i < 127; i++) {
+//			segment[i + 1] = data[i];
+//		}
+//		segment = Arrays.copyOfRange(data,0,127);
+
+
+//		if (true)
+//			throw new UnsupportedOperationException(TODO.method());
+
 		// TODO - END
 		return segment;
-		
+
 	}
 
 	public static Message decapsulate(byte[] segment) {
 
-		Message message = null;
+		Message message=null;
+		byte[] temp=new byte[segment[0]];
 		
-		// TODO - START
+//		 TODO - START
 		// decapsulate segment and put received payload data into a message
-		
-		if (true)
-			throw new UnsupportedOperationException(TODO.method());
+//		byte[] seg=segment;
+
+		for(int i=0;i<segment[0];i++){
+			temp[i]=segment[i+1];
+		}
+//		while(segment[i]!=null){
+//			temp[i-1]=segment[i];
+//			i++;
+//		}
+		message = new Message(temp);
+//		take byte[] segment as param
+//		from index 1, copy into message
+//		message.setData(Arrays.copyOfRange(segment, 1,127));
+//		wrong, copies whole array instead of just payload
+
+//		if (true)
+//			throw new UnsupportedOperationException(TODO.method());
 		
 		// TODO - END
 		
